08ce45978c84b3ccf62120c71453f7b45d1abfa4
sasahershko <sarayhershko@gmail.com>
2024-11-05 21:44:04 +0100

    Update ComicDetail.jsx

--- marvel-web/src/components/ComicDetail.jsx
+++ marvel-web/src/components/ComicDetail.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from '../styles/ComicDetail.module.css'; // Importación correcta para CSS Module
 
 export default function ComicDetail ({ comic, onClose }) {
-    if (!comic) return null; // Si no hay cómic seleccionado, no muestra nada
+    if (!comic) return null; 
 
     return (
         <div className={styles.modalBackground} onClick={onClose}>
@@ -19,6 +19,19 @@ export default function ComicDetail ({ comic, onClose }) {
                 <p><strong>Número de páginas:</strong> {comic.pageCount}</p>
                 <p><strong>Fecha de publicación:</strong> {comic.dates.find(date => date.type === "onsaleDate")?.date.slice(0, 10)}</p>
                 <p><strong>Series:</strong> {comic.series.name}</p>
+
+                <div className={styles.characterSection}>
+                    <h3>Personajes:</h3>
+                    {comic.characters?.items?.length > 0 ? (
+                        <ul>
+                            {comic.characters.items.map((character) => (
+                                <li key={character.resourceURI}>{character.name}</li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>No hay personajes disponibles para este cómic.</p>
+                    )}
+                </div>
             </div>
         </div>
     );
